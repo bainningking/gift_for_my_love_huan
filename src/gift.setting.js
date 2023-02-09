2313a6011768c4991def07776347474a5c176fd8
bainningking <bainningking@gmail.com>
2023-02-09 19:32:53 +0800

    cmt1

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -11,15 +11,17 @@ export const IS_SHOW_RESTART = false;
 // 首页配置
 export const homeConfig = {
   // 标题
-  title: '520送礼抽奖',
+  title: '紧张刺激的环仔礼物大转盘',
   // 留言，数组的每一项代表一行
   messages: [
-    { key: 'a', wording: '这是第一行文案', },
-    { key: 's', wording: '这是第二行文案', },
-    { key: 'd', wording: '然后，这里是第三行文案', },
+    { key: 'a', wording: '我自将心照白月', },
+    { key: 's', wording: '爱如潮水没首身', },
+    { key: 'd', wording: '杨柳依依情不断', },
+    { key: 'f', wording: '环宇穷宙难离分', },
+    { key: 'g', wording: '准备好了吗？紧张刺激的大转盘即将开启！', },
   ],
   // 最终解释权归属人
-  owner: 'XXX',
+  owner: '环仔的小哥',
   // 抽奖转盘的动画时间，单位毫秒
   timeout: 5000,
   // 指定赠送的礼物的索引（第几个礼物中奖），取值范围 0-8
@@ -31,12 +33,12 @@ export const homeConfig = {
 // 图片放在 public/images 中，建议是正方形，否则影响视觉体验
 // name 是礼物的全称；alias 是礼物的简称（用来显示在抽奖方格里）
 export const gifts = [
-  { key: 'q', name: 'Dior双飞套装', alias: 'Dior双飞', image: '/images/1.png',  description: '「经典迷人色彩，彰显奢华魅力」' },
-  { key: 'w', name: '腾讯视频终身VIP', alias: 'VIP', image: '/images/2.png',  description: '“为你承包一辈子的 VIP”' },
-  { key: 'e', name: '兰蔻小黑瓶套装', alias: '小黑瓶套装', image: '/images/3.png',  description: '「强维稳，快修护」' },
-  { key: 'r', name: '520红包', alias: '520', image: '/images/6.png',  description: '“想给你唱一百首情歌”' },
-  { key: 't', name: 'Dior星空套装', alias: 'Dior星空', image: '/images/5.png',  description: '「百变唇妆，精美雕琢」' },
-  { key: 'y', name: '1314红包', alias: '1314', image: '/images/6.png',  description: '“从今往后，我都会在你旁边”' },
-  { key: 'u', name: 'UR购物券：¥1,000', alias: 'UR', image: '/images/7.png',  description: '“UR！买！”' },
-  { key: 'i', name: 'Dyson美发套装', alias: '戴森', image: '/images/8.png',  description: '「不同造型需求，全面满足」' },
+  { key: 'q', name: '乐高积木花束', alias: '乐高花束', image: '/images/hua.png',  description: '送你一束不会凋零的花' },
+  { key: 'w', name: '京东plus会员（永久）', alias: '京东vip', image: '/images/plus.jpg',  description: '承包你一辈子的vip' },
+  { key: 'e', name: '美美哒全身套装', alias: '穿上你就美美哒', image: '/images/qunzi.jpg',  description: '怎么美，怎么穿' },
+  { key: 'r', name: '520红包', alias: '520，每一天', image: '/images/520.png',  description: '我爱你，宝宝' },
+  { key: 't', name: '动物园一日游', alias: '看大脑斧', image: '/images/dongwuyuan.png',  description: '想陪你看遍世界' },
+  { key: 'y', name: '1314红包', alias: '爱你，1314', image: '/images/1314.png',  description: '一生一世，我都会在你旁边' },
+  { key: 'u', name: 'SKⅡ神仙水5步装', alias: 'SKⅡ', image: '/images/sk.jpg',  description: 'SKⅡ？买！' },
+  { key: 'i', name: '0.52红包', alias: '倒过来也是520', image: '/images/052.png',  description: '红包再小也是爱' },
 ];
