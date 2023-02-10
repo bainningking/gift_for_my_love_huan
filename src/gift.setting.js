33e792c862a97e00778cd58bb09128018a7bd6ac
bainningking <bainningking@gmail.com>
2023-02-10 17:24:32 +0800

    cmt18

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -33,12 +33,12 @@ export const homeConfig = {
 // 图片放在 public/images 中，建议是正方形，否则影响视觉体验
 // name 是礼物的全称；alias 是礼物的简称（用来显示在抽奖方格里）
 export const gifts = [
-  { key: 'q', name: '乐高积木花束', alias: '乐高花束', image: '/images/hua.png',  description: '送你一束不会凋零的花' },
-  { key: 'w', name: '京东plus会员（永久）', alias: '京东vip', image: '/images/plus.jpg',  description: '承包你一辈子的vip' },
-  { key: 'e', name: '美美哒全身套装', alias: '穿上你就美美哒', image: '/images/qunzi.jpg',  description: '怎么美，怎么穿' },
-  { key: 'r', name: '520红包', alias: '520，每一天', image: '/images/520.png',  description: '我爱你，宝宝' },
-  { key: 't', name: '动物园一日游', alias: '看大脑斧', image: '/images/dongwuyuan.png',  description: '想陪你看遍世界' },
-  { key: 'y', name: '1314红包', alias: '爱你，1314', image: '/images/1314.png',  description: '一生一世，我都会在你旁边' },
-  { key: 'u', name: 'SKⅡ神仙水5步装', alias: 'SKⅡ', image: '/images/sk.jpg',  description: 'SKⅡ？买！' },
-  { key: 'i', name: '0.52红包', alias: '倒过来也是520', image: '/images/052.png',  description: '红包再小也是爱' },
+  { key: 'q', name: '乐高积木花束', alias: '乐高花束', image: '/gift_for_my_love_huan/images/hua.png',  description: '送你一束不会凋零的花' },
+  { key: 'w', name: '京东plus会员（永久）', alias: '京东vip', image: '/gift_for_my_love_huan/images/plus.jpg',  description: '承包你一辈子的vip' },
+  { key: 'e', name: '美美哒全身套装', alias: '穿上你就美美哒', image: '/gift_for_my_love_huan/images/qunzi.jpg',  description: '怎么美，怎么穿' },
+  { key: 'r', name: '520红包', alias: '520，每一天', image: '/gift_for_my_love_huan/images/520.png',  description: '我爱你，宝宝' },
+  { key: 't', name: '动物园一日游', alias: '看大脑斧', image: '/gift_for_my_love_huan/images/dongwuyuan.png',  description: '想陪你看遍世界' },
+  { key: 'y', name: '1314红包', alias: '爱你，1314', image: '/gift_for_my_love_huan/images/1314.png',  description: '一生一世，我都会在你旁边' },
+  { key: 'u', name: 'SKⅡ神仙水5步装', alias: 'SKⅡ', image: '/gift_for_my_love_huan/images/sk.jpg',  description: 'SKⅡ？买！' },
+  { key: 'i', name: '0.52红包', alias: '倒过来也是520', image: '/gift_for_my_love_huan/images/052.png',  description: '红包再小也是爱' },
 ];
